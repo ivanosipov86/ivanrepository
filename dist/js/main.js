9b07597600c40018f7099e2c14da4648dcfdbf78
ivanosipov86 <Enotpalaskvn@gmail.com>
2020-03-25 19:21:45 +0300

    add changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,32 @@
+/*
 do{
     var num1 = +prompt('первое число');
     var num2 = +prompt('второе число');
     var result = num1 + num2;
     alert('результат:' + result);
     var isProcess = confirm('Продолжить?');
-} while( isProcess);
+} while( isProcess);*/
 
 function getValues (){
     var result = [];
-    var i=0;
+    var i = 0;
+
     do{
-        result[i]=+prompt('Введите число');
-        i++;
+        result[i++] = +prompt('Введите число');
         var isProcess = confirm('Продолжить?');
-    }while(isProcess)
+    }while(isProcess);
+
     return result;
-}
+} 
 do {
-    var
+    var numbers = getValues();
+    var res = numbers[0];
+    
+    for(var i = 1; i <numbers.length; i++) {
+        res = res + numbers[i];
 }
+
+alert('Итого:' + res);
+
+var isProcess = confirm('Продолжить');
+} while (isProcess);
