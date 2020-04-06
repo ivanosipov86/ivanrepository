e91a8627e323c734335fa9ae773fbf9c1b070f60
ivanosipov86 <enotpalaskvn@gmail.com>
2020-04-06 20:28:26 +0300

    add slick slider

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,19 +1,7 @@
-$(function() {
-  // Owl Carousel
-  var owl = $(".owl-carousel");
-  owl.owlCarousel({
-    items: 3,
-    margin: 10,
-    loop: true,
-    nav: true
-  });
-  
-});
-$(".next_button").click(function() {
-  owl.trigger("next.owl.carousel");
-});
-
-$(".prev_button").click(function() {
-  owl.trigger("prev.owl.carousel");
-});
+$(document).ready(function(){
+  $('.client__reviews').slick({
+    
+    
+  })
+})
 
