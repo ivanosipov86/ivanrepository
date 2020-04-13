7b48901cee5c0c2a1b5215f81b029a004a5885b9
ivanosipov86 <enotpalaskvn@gmail.com>
2020-04-13 21:03:04 +0300

    add slick

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-  $('.client__reviews').slick({
-    
-    
-  })
-})
-
+ $('.download__slider').slick({
+  
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  vertical: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  infinite: true,
+ });
+});
